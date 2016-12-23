ac67c6ec7e6007c03a04849885648ffbd03bde79
Flakebi <flakebi@t-online.de>
2016-12-23 16:51:57 +0100

    Use eto instead of xwt for better mac support

---
+++ Zeltlager/Zeltlager.Client/MainForm.xeto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+
+namespace Zeltlager.Client
+{
+	using Network;
+	using Requests = CommunicationPackets.Requests;
+	using Responses = CommunicationPackets.Responses;
+
+	public class MainForm : Form
+	{
+		public MainForm()
+		{
+			XamlReader.Load(this);
+		}
+
+		protected void Connect(object sender, EventArgs e)
+		{
+			MessageBox.Show("I was clicked!");
+		}
+
+		protected void ListLagers(object sender, EventArgs e)
+		{
+			MessageBox.Show("I was clicked!");
+		}
+
+		protected void HandleQuit(object sender, EventArgs e)
+		{
+			Application.Instance.Quit();
+		}
+	}
+}
