61d0c7841bd8fc8cad975021a812048a1125247d
Flakebi <flakebi@t-online.de>
2017-03-08 19:43:26 +0100

    Add a new IoProvider - not yet working

--- UnitTests/BigLagerTest.cs
+++ UnitTests/BigLagerTest.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using Zeltlager;
+using Zeltlager.DataPackets;
 
 namespace UnitTests
 {
 	public class BigLagerTest : LagerTest
 	{
-		public BigLagerTest()
+		[Test]
+		public void BigTest()
 		{
+			Task.WaitAll(BigTestAsync());
+		}
+
+		public async Task BigTestAsync()
+		{
+			await Init();
+			// Save the lager
+			// Load the lager again
 		}
 	}
 }
