66dce1083bd9218e2085e70f0c2e59d60214ee8a
s <s@MIRACULIX-WIN>
2017-03-22 16:29:14 +0100

    Insert tabs into the competition page

--- Zeltlager/Zeltlager/Competition/CompetitionPages/CompetitionPage.xaml.cs
+++ Zeltlager/Zeltlager/Competition/CompetitionPages/CompetitionPage.xaml.cs
@@ -1,13 +1,15 @@
 using System;
-using Xamarin.Forms;
-using Zeltlager.UAM;
-using Zeltlager.Client;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Xamarin.Forms;
+
 namespace Zeltlager.Competition
 {
-	public partial class CompetitionPage : ContentPage
+	using Client;
+	using UAM;
+
+	public partial class CompetitionPage : TabbedPage
 	{
 		Competition competition;
 		LagerClient lager;
@@ -18,78 +20,33 @@ namespace Zeltlager.Competition
 			this.competition = competition;
 			this.lager = lager;
 
+			BindingContext = competition;
 			NavigationPage.SetBackButtonTitle(this, "");
 			UpdateUI();
 		}
 
 		void UpdateUI()
 		{
-			StackLayout vsl = new StackLayout
-			{
-				Orientation = StackOrientation.Vertical,
-				HorizontalOptions = LayoutOptions.FillAndExpand,
-				VerticalOptions = LayoutOptions.FillAndExpand,
-				Padding = new Thickness(10, 2)
-			};
+			var stationList = new SearchableListView<Station>(
+				competition.Stations.Where(s => s.IsVisible).ToList(),
+				OnEditClickedStation, OnDeleteClickedStation, OnStationClicked);
 
-			StackLayout stationHeader = new StackLayout
-			{
-				Orientation = StackOrientation.Horizontal
-			};
-			stationHeader.Children.Add(new Label
-			{
-				Text = "Stationen",
-				HorizontalOptions = LayoutOptions.CenterAndExpand,
-				VerticalOptions = LayoutOptions.Center
-			});
-			Button addStation = new Button { Image = Icons.ADD, HorizontalOptions = LayoutOptions.End };
-			addStation.Clicked += OnAddStationClicked;
-			stationHeader.Children.Add(addStation);
+			var participantList = new SearchableListView<Participant>(
+				competition.Participants.Where(p => p.IsVisible).ToList(),
+				OnEditClickedParticipant, OnDeleteClickedParticipant, OnParticipantClicked);
 
-			var stationList = new SearchableListView<Station>(competition.Stations.Where(s => s.IsVisible).ToList(),
-			                                                  OnEditClickedStation, OnDeleteClickedStation, OnStationClicked);
-
-			StackLayout participantHeader = new StackLayout
-			{
-				Orientation = StackOrientation.Horizontal
-			};
-			participantHeader.Children.Add(new Label
-			{
-				Text = "Teilnehmer",
-				HorizontalOptions = LayoutOptions.CenterAndExpand,
-				VerticalOptions = LayoutOptions.Center
-			});
-			Button addParticipant = new Button { Image = Icons.ADD, HorizontalOptions = LayoutOptions.End };
-			addParticipant.Clicked += OnAddParticipantClicked;
-			participantHeader.Children.Add(addParticipant);
-
-			var participantList = new SearchableListView<Participant>(competition.Participants.Where(p => p.IsVisible).ToList(),
-			                                                          OnEditClickedParticipant, OnDeleteClickedParticipant, OnParticipantClicked);
-
-			vsl.Children.Add(stationHeader);
-			vsl.Children.Add(stationList);
-			vsl.Children.Add(participantHeader);
-			vsl.Children.Add(participantList);
-
-			Content = vsl;
+			stationPage.Content = stationList;
+			participantPage.Content = participantList;
 		}
 
-		//void OnStationSelected(object sender, SelectedItemChangedEventArgs e)
-		//{
-		//	//ItemSelected is called on deselection, which results in SelectedItem being set to null
-		//	if (e.SelectedItem == null)
-		//		return;
-		//	Navigation.PushAsync(new NavigationPage(new StationPage()));
-		//}
-
-		void OnAddStationClicked(object sender, EventArgs e)
+		void OnAddButtonClicked(object sender, EventArgs e)
 		{
-			Navigation.PushModalAsync(new NavigationPage(new UniversalAddModifyPage<Station, Rankable>(new Station(null, "", competition), true, lager)), true);
-		}
-
-		void OnAddParticipantClicked(object sender, EventArgs e)
-		{
-			Navigation.PushModalAsync(new NavigationPage(new AddEditParticipantPage(new GroupParticipant(null, "", competition), true)), true);
+			Page addPage;
+			if (CurrentPage == stationPage)
+				addPage = new UniversalAddModifyPage<Station, Rankable>(new Station(null, "", competition), true, lager);
+			else
+				addPage = new AddEditParticipantPage(new GroupParticipant(null, "", competition), true);
+			Navigation.PushModalAsync(new NavigationPage(addPage), true);
 		}
 
 		protected override void OnAppearing()
@@ -97,8 +54,6 @@ namespace Zeltlager.Competition
 			base.OnAppearing();
 			UpdateUI();
 		}
-
-		#region Searchable implementaition
 
 		void OnStationClicked(Station station)
 		{
@@ -131,6 +86,5 @@ namespace Zeltlager.Competition
 			await participant.Delete(lager);
 			OnAppearing();
 		}
-		#endregion
 	}
 }
